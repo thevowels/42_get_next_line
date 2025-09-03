141080d8913f538286f797dea7679b4b91bc00a7
Aung Phyo Htet <aphyo-ht@z2t14c1.42bangkok.com>
2025-09-04 06:35:46 +0700

    getting the constant from the compile time

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 05:51:22 by aphyo-ht          #+#    #+#             */
+/*   Updated: 2025/09/04 06:29:04 by aphyo-ht         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+#endif
