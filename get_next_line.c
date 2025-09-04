a151091b8a138407b8c9bfec5f260a9d6c7781f2
Aung Phyo Htet <aphyo-ht@z2t14c1.42bangkok.com>
2025-09-04 07:23:52 +0700

    read ok . Need to find a way to track the characters after \n for next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,70 @@
 /*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 05:47:38 by aphyo-ht          #+#    #+#             */
-/*   Updated: 2025/09/04 06:04:40 by aphyo-ht         ###   ########.fr       */
+/*   Updated: 2025/09/04 07:16:23 by aphyo-ht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "get_next_line.h"
+#include <stdio.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < size -1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (0);
+}
+
+
 
 char	*get_next_line(int fd)
-{
-	char *buffer = malloc(50);
-
-	while(read(fd, buffer, 20));
+{	
+	size_t	length;
+	size_t	index;
+	char	*buffer;
+	char	*result;
+	
+	result = malloc(1000);
+	index = 0;
+	
+	buffer = malloc(BUFFER_SIZE);
+	length = read(fd, buffer, BUFFER_SIZE);
+	if(!length)
+		return (NULL);
+	while(length)
+	{
+		ft_strlcpy(&result[index], buffer, length+1);
+		index += length;
+		if(ft_strchr(buffer, '\n'))
+			return (result);
+		length = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (result);
 }
--- main.c
+++ main.c
@@ -6,18 +6,29 @@
 /*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 06:04:51 by aphyo-ht          #+#    #+#             */
-/*   Updated: 2025/09/04 06:29:07 by aphyo-ht         ###   ########.fr       */
+/*   Updated: 2025/09/04 07:22:47 by aphyo-ht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-// char	*get_next_line(int fd);
+char	*get_next_line(int fd);
 
 int main(void)
 {
-	printf("BUFFER_SIZE : %d\n", BUFFER_SIZE);
+	int	fd;
+	char *result;
+
+	fd = open("main.c", O_RDONLY);
+
+	while((result = get_next_line(fd)))
+	{
+		printf("%s", result);
+		printf("\n---------------------\n");
+	}
+
+	printf("Buffer size: %d\n", BUFFER_SIZE);
 
 	return (1);
 
